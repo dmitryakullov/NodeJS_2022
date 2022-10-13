de002897d6966227732d47ee9eba51dff4f2ad2b
Dmitry Akulov <dima224xz@gmail.com>
2022-10-13 13:35:20 +0300

    feat: improve security

--- auth/auth.js
+++ auth/auth.js
@@ -4,6 +4,7 @@ const JWTstrategy = require('passport-jwt').Strategy;
 const ExtractJWT = require('passport-jwt').ExtractJwt;
 const { signupHandler, loginHandler } = require('../utils/databaseUtils');
 const { JWT_SALT } = require('../constants/salt');
+const { UsersDb } = require('../db');
 
 passport.use(
   'signup',
@@ -35,7 +36,12 @@ passport.use(
     },
     (token, done) => {
       try {
-        return done(null, token);
+        UsersDb.findOne({ _id: token._id }, (err, user) => {
+          if (err || !user?.email) {
+            return done(err);
+          }
+          done(null, token);
+        });
       } catch (error) {
         done(error);
       }
